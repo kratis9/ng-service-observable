d200cd0dc4aa29bd8e71f839d453e6227b22c7ae
krati <kratis9@gmail.com>
2017-12-06 06:17:36 +1100

    add error handeling in service

--- src/app/todo.service.ts
+++ src/app/todo.service.ts
@@ -1,13 +1,20 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient,HttpErrorResponce} from '@angular/common/http';
 import {Observable} from 'rxjs/Observable';
+import  'rxjs/add/operator/catch';
 import {ITodo} from './todo';
+import { error } from 'selenium-webdriver';
 @Injectable()
 export class TodoService {
 private _url='https://jsonplaceholder.typicode.com/todos';
   constructor(private _http:HttpClient){
   }
   getTodoList(): Observable<ITodo[]> {
-    return this._http.get<ITodo[]>(this._url);
+    return this._http.get<ITodo[]>(this._url)
+    .catch(this.handleError);
   }
+private handleError(err:HttpErrorResponce){
+console.log(err);
+
+}
 }
